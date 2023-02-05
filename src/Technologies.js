452b61e2105e61c78e5f2214fdaf7dbf96e274b3
magomedov777 <alibullterrier@gmail.com>
2023-02-05 17:08:45 +0200

    typescript

---
+++ src/Technologies.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+
+
+const Technologies = () => {
+    return(
+      <div>
+        <ul>
+          <li>HTML</li>
+          <li>CSS</li>
+          <li>JS</li>
+          <li>REACT</li>
+        </ul>
+      </div>
+    )
+  };
+
+
+
+  export default Technologies;
