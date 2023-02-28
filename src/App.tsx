ba40c3407a90f03f2dbb7e180a6103748b227adc
Arkadiy <alibullterrier@gmail.com>
2023-02-28 14:43:45 +0200

    all files

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,48 @@ import './App.css';
 
 const App = () => {
   return (
-    <div>
-      <header>
+    <div className='app-wrapper'>
+      <header className='header'>
         <img src='https://blog.hubspot.com/hs-fs/hubfs/image8-2.jpg?width=600&name=image8-2.jpg'/>
       </header>
-      <nav>
-        <div>Profile</div>
-        <div>Messages</div>
-        <div>News</div>
-        <div>Music</div>
+      <nav className='nav'>
+        <div>
+          <a>Profile</a>
+        </div>
+        <div>
+          <a>Messages</a>
+        </div>
+        <div>
+          <a>News</a>
+        </div>
+        <div>
+          <a>Music</a>
+        </div>
+        <div>
+          <a>Settings</a>
+        </div>
+       
       </nav>
+      <div className='content'>
+    <div>
+        <img src='https://www.gannett-cdn.com/presto/2018/08/14/PTAL/6e4fff76-595d-4069-9112-cfe15dbfaa43-IMG_Stadium.jpeg?width=660&height=319&fit=crop&format=pjpg&auto=webp'/>
+    </div>
+    <div>
+      avatar + description
+    </div>
+    <div>
+      My posts
+      <div>
+        New post
+      </div>
+      <div>
+        Post 1
+      </div>
+      <div>
+        Post 2
+      </div>
+    </div>
+      </div>
     
     </div>
   );
