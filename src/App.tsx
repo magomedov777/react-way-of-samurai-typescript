b7d606583ccf6da4ad3e5b6e6ee51f05ed5d2407
Arkadiy <alibullterrier@gmail.com>
2023-02-28 14:09:14 +0200

    new logo

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import './App.css';
 const App = () => {
   return (
     <div>
+      <header><img src='https://blog.hubspot.com/hs-fs/hubfs/image8-2.jpg?width=600&name=image8-2.jpg'/></header>
     
     </div>
   );
