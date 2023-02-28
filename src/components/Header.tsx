7c74080abdc9c36e2e9261253a85234c37360744
Arkadiy <alibullterrier@gmail.com>
2023-02-28 16:52:34 +0200

    new css components

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,18 +1,13 @@
 import React from "react";
-
-
+import './Header.css';
 
 const Header = () => {
   return (
          <header className='header'>
           <img src='https://blog.hubspot.com/hs-fs/hubfs/image8-2.jpg?width=600&name=image8-2.jpg'/>
         </header>
-  )
-}
-
-
-
-
+  );
+};
 
 
 
