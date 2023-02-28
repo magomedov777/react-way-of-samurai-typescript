a9ce1bb25a9b6885b6436e9ce995a96bd286988b
Arkadiy <alibullterrier@gmail.com>
2023-02-28 14:11:58 +0200

    new test

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,15 @@ import './App.css';
 const App = () => {
   return (
     <div>
-      <header><img src='https://blog.hubspot.com/hs-fs/hubfs/image8-2.jpg?width=600&name=image8-2.jpg'/></header>
+      <header>
+        <img src='https://blog.hubspot.com/hs-fs/hubfs/image8-2.jpg?width=600&name=image8-2.jpg'/>
+      </header>
+      <nav>
+        <div>Profile</div>
+        <div>Messages</div>
+        <div>News</div>
+        <div>Music</div>
+      </nav>
     
     </div>
   );
