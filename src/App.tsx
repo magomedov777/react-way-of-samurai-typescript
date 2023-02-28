885a4dc41fd25882be268aa4b227c82a58311901
Arkadiy <alibullterrier@gmail.com>
2023-02-28 16:19:41 +0200

    new components

--- src/App.tsx
+++ src/App.tsx
@@ -2,33 +2,15 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header';
 import Navbar from './components/Navbar';
+import Profile from './components/Profile';
 
 
 const App = () => {
   return (
     <div className='app-wrapper'>
        <Header/>
-          <Navbar/>
-          <div className='content'>
-           <div>
-            <img src='https://www.gannett-cdn.com/presto/2018/08/14/PTAL/6e4fff76-595d-4069-9112-cfe15dbfaa43-IMG_Stadium.jpeg?width=660&height=319&fit=crop&format=pjpg&auto=webp'/>
-           </div>
-           <div>
-             avatar + description
-           </div>
-            <div>
-             My posts
-            <div>
-             New post
-            </div>
-            <div>
-              Post 1
-            </div>
-            <div>
-              Post 2
-            </div>
-      </div>
-    </div>
+        <Navbar/>
+         <Profile/>
   </div>
   );
 };
---
+++ src/components/Profile.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+
+const Profile = () => {
+  return (
+    <div className='content'>
+           <div>
+            <img src='https://www.gannett-cdn.com/presto/2018/08/14/PTAL/6e4fff76-595d-4069-9112-cfe15dbfaa43-IMG_Stadium.jpeg?width=660&height=319&fit=crop&format=pjpg&auto=webp'/>
+           </div>
+           <div>
+             avatar + description
+           </div>
+            <div>
+             My posts
+            <div>
+             New post
+            </div>
+            <div>
+              Post 1
+            </div>
+            <div>
+              Post 2
+            </div>
+      </div>
+    </div>
+  )
+}
+
+
+
+
+
+export default Profile;
