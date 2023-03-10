e0142f1d2155de367aef4f220edc6044d2abebd1
Arkadiy <alibullterrier@gmail.com>
2023-03-10 17:13:24 +0200

    new

--- src/App.tsx
+++ src/App.tsx
@@ -4,20 +4,23 @@ import Dialogs from './components/Dialogs';
 import Header from './components/Header';
 import Navbar from './components/Navbar';
 import Profile from './components/Profile';
+import {BrowserRouter, Route} from 'react-router-dom';
+
 
 
 
 const App = () => {
   return (
+    <BrowserRouter>
     <div className='app-wrapper'>
        <Header/>
         <Navbar/>
-         {/* <Profile/> */}
-         <div className='app-wrapper-content'>
-          <Dialogs/>
-         </div>
-          
-  </div>
+        <div className='app-wrapper-content'>
+          <Route path="/dialogs" component={Dialogs}/>
+          <Route path="/profile" component={Profile}/>
+          </div>
+        </div>
+     </BrowserRouter>
   );
 };
 
