e4e0bf4bb7062730cb33dafd2a3ef526a22a8218
Arkadiy <alibullterrier@gmail.com>
2023-03-14 10:16:20 +0200

    end ref

--- src/components/Dialogs.tsx
+++ src/components/Dialogs.tsx
@@ -47,18 +47,10 @@ let messagesElements = messages.map(m => <Message message={m.message}/>)
   return (
     <div className={s.dialogs}>
      <div className={s.dialogsItems}>
-      <DialogItem name={dialogs[0].name} id={dialogs[0].id}/>
-      <DialogItem name={dialogs[1].name} id={dialogs[1].id}/>
-      <DialogItem name={dialogs[2].name} id={dialogs[2].id}/>
-      <DialogItem name={dialogs[3].name} id={dialogs[3].id}/>
-      <DialogItem name={dialogs[4].name} id={dialogs[4].id}/>
-      <DialogItem name={dialogs[5].name} id={dialogs[5].id}/>
-      {dialogsElements}
+       {dialogsElements}
      </div>
        <div className={s.messages}>
-        <Message message={messages[0].message}/>
-        <Message message={messages[1].message}/>
-        <Message message={messages[2].message}/>
+        {messagesElements}
        </div>
     </div>
   );
