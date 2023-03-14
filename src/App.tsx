2ada3943ee1d64ddda8731c31e04df0283ff6fc8
Arkadiy <alibullterrier@gmail.com>
2023-03-14 16:14:39 +0200

    route render

--- src/App.tsx
+++ src/App.tsx
@@ -16,8 +16,8 @@ const App = () => {
        <Header/>
         <Navbar/>
         <div className='app-wrapper-content'>
-          <Route path="/dialogs" component={Dialogs}/>
-          <Route path="/profile" component={Profile}/>
+          <Route path="/dialogs" render={ () => <Dialogs name='' id={1}/>}/>
+          <Route path="/profile" render={ () => <Profile/> }/>
           </div>
         </div>
      </BrowserRouter>
